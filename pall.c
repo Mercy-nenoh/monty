6e7006eb7bdb31718805b3982ff16a413995fc1a
Mercy-nenoh <mercynenoh94@gmail.com>
2022-09-07 03:13:10 -0700

    The Momty Language

---
+++ pall.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * pall - prints the stack
+ * @stack: stack given by main in start.c
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	print_stack(*stack);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privac
